dd1ce15f8f2ac519b26c691a2b535212a3b2d9df
ABHILASH <99235816+M4X1M4S@users.noreply.github.com>
2024-10-13 17:51:31 +0530

    Implement cookies and session with Passport local strategy

---
+++ index4.js
@@ -0,0 +1,162 @@
+import express from "express";
+import bodyParser from "body-parser";
+import env from "dotenv";
+import pg from "pg";
+import passport from "passport";
+import { Strategy } from "passport-local";
+import session from "express-session";
+import bcrypt from "bcrypt";
+
+
+
+
+const app = express();
+const port = 3000;
+env.config();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.use(
+  session({
+  secret:"MYTOPSECRET",
+  resave:false,
+  saveUninitialized:true,
+})
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+const db= new pg.Client({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT
+});
+
+db.connect();
+
+// Function to hash a password
+async function hashPassword(password) {
+  const saltRounds = 10; // Number of rounds to generate salt
+  const hashedPassword = await bcrypt.hash(password, saltRounds); // Hash the password
+  return hashedPassword;
+}
+
+// Function to verify a password
+async function verifyPassword(password, hashedPassword) {
+  const match = await bcrypt.compare(password, hashedPassword); // Compare plain password with hashed password
+  return match; // Returns true if the password matches
+}
+
+
+app.get("/", (req, res) => {
+  res.render("home.ejs");
+});
+
+app.get("/login", (req, res) => {
+  res.render("login.ejs");
+});
+
+app.get("/register", (req, res) => {
+  res.render("register.ejs");
+});
+
+app.get('/secrets',(req,res)=>{
+
+  //console.log(req.user)
+  if (req.isAuthenticated){
+    res.render("secrets.ejs");
+  }
+  else{
+    res.render("login.ejs");
+  }
+})
+
+app.post("/register", async (req, res) => {
+  try {
+    const result= await db.query("SELECT * FROM users WHERE email = $1",[req.body.username]);
+  if(result.rows.length>0){
+    res.send("User already registered , try logging in");
+    res.render('login.ejs');
+  }
+  else{
+    const hashedPassword= hashPassword(req.body.password);
+    await db.query("INSERT INTO users (email,password) VALUES ($1,$2)",[req.body.username,hashedPassword]);
+    res.render('home.ejs');
+  }
+  } catch (error) {
+    console.log("Error registering User "+ error);
+  }
+  
+});
+app.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/secrets",
+    failureRedirect: "/login",
+   
+  })
+);
+
+
+// app.post("/login", async (req, res) => {
+//   try {
+//     const result= await db.query("SELECT * FROM users WHERE email = $1",[req.body.username]);
+//     if(result.rows.length>0){
+//       if(result.rows[0].password===req.body.password){
+//         res.render('secrets.ejs');
+//       }
+//       else{
+//         res.send("Incorrect Password , try Again");
+     
+//       }
+//     }
+//     else{
+//       res.send("NO user found try register again");
+     
+//     }
+//   } catch (error) {
+//     console.log("Error logging in" + error)
+//   }
+
+// });
+
+passport.use(
+  new Strategy (async function verify(username,password,cb){ //verify gets the username and password from the form which submits that
+    try {
+      const result= await db.query("SELECT * FROM users WHERE email = $1",[username]);
+      const user=result.rows[0];
+
+      if(result.rows.length>0){
+          
+        if(verifyPassword(password,result.rows[0].password)){
+           return cb(null,user);
+        }
+        else{
+          return cb(null,false);
+       
+        }
+      }
+      else{
+        return cb("NO user found try register again");
+       
+      }
+    } catch (error) {
+      return cb(error);
+    }
+  })
+)
+
+passport.serializeUser((user,cb)=>{
+  cb(null,user);
+});
+
+passport.deserializeUser((user,cb)=>{
+  cb(null,user);
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
